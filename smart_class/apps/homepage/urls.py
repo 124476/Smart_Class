16318fb38d9894f4abd6162919cd2d38376eebbe
mario12508 <mario12508@mail.ru>
2025-01-29 23:29:17 +0300

    complete urls to other sites and games setups

--- smart_class/apps/homepage/urls.py
+++ smart_class/apps/homepage/urls.py
@@ -6,6 +6,4 @@ app_name = "homepage"
 urlpatterns = [
     path("", apps.homepage.views.Home.as_view(), name="main"),
     path("games/", apps.homepage.views.Games.as_view(), name="games"),
-    path("tatarlango/", apps.homepage.views.Tatarlango.as_view(), name="tatarlango"),
-    path("entangled_tale/", apps.homepage.views.EntangledTale.as_view(), name="entangled_tale"),
 ]
